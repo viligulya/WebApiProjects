7185993ec27108f0a26e78465f195f0998b06785
Eu9ep1 <vilmos.gulya@stud.ui>
2023-11-29 16:28:00 +0100

    10.gyak

---
+++ hajos/wwwroot/geek.js
@@ -0,0 +1,39 @@
+﻿
+
+var viccek;
+var kérdések;
+
+window.onload = letöltés
+
+function letöltés() {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(kérdések => letöltésBefejeződött(kérdések)
+
+        );
+}
+
+
+
+
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+    for (var i = 0; i < viccek.length; i++) {
+        var ujsor = document.createElement("h1")
+        ujsor.innerText = i;
+       
+    }
+    
+
+}
+
+
+
+
+
+
+
+
